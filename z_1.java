16611ebe2a6de5c8afe087aaddbbf58a22a5de49
Egorrito <73782293+Egorrito@users.noreply.github.com>
2021-01-09 16:15:00 +0700

    Add files via upload

---
+++ z_1.java
@@ -0,0 +1,10 @@
+
+package l_2;
+
+public class z_1 {
+    
+public static void main(String[] args) {
+System.out.println ("а=1, c=a+1 m=2, n=3 решение уравнений");
+System.out.println("b=6, d=b*2-1 p=4, g=5 нахождение корней");
+}
+}
---
+++ z_2.java
@@ -0,0 +1,11 @@
+package l_2;
+
+public class z_2 {
+
+public static void main(String[] args) {
+System.out.println("        1");
+System.out.println("      1 2 3");
+System.out.println("    1 2 3 4 5");
+System.out.println("  1 2 3 4 5 6 7");
+    }
+}
